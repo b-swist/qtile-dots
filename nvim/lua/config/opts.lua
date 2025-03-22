e444fd2f810de537dd0e870ccc35f2d9d144f8d5
b-swist <bswist@protonmail.com>
2025-03-22 12:49:45 +0100

    set termguicolors to true, set smartindent to false in tex files

--- nvim/lua/config/opts.lua
+++ nvim/lua/config/opts.lua
@@ -25,11 +25,14 @@ vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+vim.opt.termguicolors = true
+
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "tex", "html" },
     callback = function()
         vim.opt_local.tabstop = 2
         vim.opt_local.softtabstop = 2
         vim.opt_local.shiftwidth = 2
+        vim.opt_local.smartindent = false
     end
 })
